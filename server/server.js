8015f9aaa2e94710d939267557e6d5d6fb094493
Zack Anselm <zanselm5@gmail.com>
2020-04-05 14:06:04 -0400

    Attempt to serve static files

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, './backend/carebackend/django-static/')));
+
+app.listen(8009, () => console.log('Client app listening on port 8009!'));
