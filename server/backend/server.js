d8a06e4776210aa98d549465179c2c426f828b31
Zack Anselm <zanselm5@gmail.com>
2020-04-05 14:30:25 -0400

    Moved stuff

--- server/backend/server.js
+++ server/backend/server.js
@@ -2,6 +2,6 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, './backend/carebackend/django-static/')));
+app.use(express.static(path.join(__dirname, './carebackend/django-static/')));
 
 app.listen(8009, () => console.log('Client app listening on port 8009!'));
