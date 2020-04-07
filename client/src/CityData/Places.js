4fa616e41b8ebfc1a4c2d27b83626f41f32c380b
Zack Anselm <zanselm5@gmail.com>
2020-04-07 12:15:28 -0400

    Updated places for suggestions

--- client/src/CityData/Places.js
+++ client/src/CityData/Places.js
@@ -45,7 +45,7 @@ const INDYPlaces = [{
   "image_attribution": "Barbecue and Bourbon"
 }, {
   "name": "EL Azabache Mexican Restaurant",
-  "address": "4200 S East St Suite 11, Indianapolis, IN 46227, USA",
+  "address": "4200 S East St Suite 11, Indianapolis, IN 46227,USA",
   "key": "ChIJs1PFq7dba4gRHJCNglK1q1M",
   "image_attribution": "['<a href=\"https://maps.google.com/maps/contrib/116633821093433127848\">Robin Storbakken</a>']"
 }, {
@@ -53,5 +53,65 @@ const INDYPlaces = [{
   "address": "3915 Madison Ave #6, Indianapolis, IN 46227, USA",
   "key": "ChIJWUWBvQFba4gRwIll8bEq2LM",
   "image_attribution": "['<a href=\"https://maps.google.com/maps/contrib/104142644533422008377\">Don Juan Taqueria</a>']"
+}, {
+  "name": "Mother Bear's Pizza Campus",
+  "address": "1428 E 3rd St, Bloomington, IN 47401, USA",
+  "key": "ChIJy5Oy8pVmbIgRa7USqK8P49E",
+  "image_attribution": "['<a href=\"https://maps.google.com/maps/contrib/108939533290925700002\">Mother Bear&#39;s Pizza Campus</a>']"
+}, {
+  "name": "Butch's Grillacatessen & Eatzeria",
+  "address": "120 E 7th St, Bloomington, IN 47408, USA",
+  "key": "ChIJFURtT9xmbIgRM-KcfLUxq2Q",
+  "image_attribution": "['<a href=\"https://maps.google.com/maps/contrib/108962660229971188222\">James Michael</a>']"
+}, {
+  "name": "Opie Taylor's",
+  "address": "110 N Walnut St, Bloomington, IN 47404, USA",
+  "key": "ChIJ9-AavN1mbIgRAu-22sim2hY",
+  "image_attribution": "['<a href=\"https://maps.google.com/maps/contrib/103168397873312886611\">Keith Carlson</a>']"
+}, {
+  "name": "Half Liter BBQ & Beer Hall",
+  "address": "5301 Winthrop Ave suite b, Indianapolis, IN 46220, USA",
+  "key": "ChIJ3a9pD1xTa4gRZGLjHLX4kCk",
+  "image_attribution": "['<a href=\"https://maps.google.com/maps/contrib/110472494963842443074\">Bo Turner</a>']"
+}, {
+  "name": "Sakura",
+  "address": "7201 N Keystone Ave, Indianapolis, IN 46240, USA",
+  "key": "ChIJK3CstfFSa4gRzCUTrK4WgRg",
+  "image_attribution": "['<a href=\"https://maps.google.com/maps/contrib/105204331758652972687\">Sakura</a>']"
+}, {
+  "name": "Mama Carolla's",
+  "address": "1031 E 54th St, Indianapolis, IN 46220, USA",
+  "key": "ChIJnycfNslTa4gRnhPSRbg0aq8",
+  "image_attribution": "['<a href=\"https://maps.google.com/maps/contrib/116096000200286008977\">Dan Huber</a>']"
+}, {
+  "name": "Condado Tacos",
+  "address": "834 Broad Ripple Ave, Indianapolis, IN 46220, USA",
+  "key": "ChIJgSM0ta1Ta4gRJPxmi1h3i84",
+  "image_attribution": "['<a href=\"https://maps.google.com/maps/contrib/114032680214715383544\">Condado Tacos</a>']"
+}, {
+  "name": "Flatwater",
+  "address": "832 E Westfield Blvd, Indianapolis, IN 46220, USA",
+  "key": "ChIJCebHS69Ta4gRRiQn89iF3XI",
+  "image_attribution": "['<a href=\"https://maps.google.com/maps/contrib/111974823830850588470\">Katherine Mayerovitch</a>']"
+}, {
+  "name": "Asaka Japanese Restaurant",
+  "address": "6414 E 82nd St, Indianapolis, IN 46250, USA",
+  "key": "ChIJXTFhvyhNa4gR_bink0ZUlcw",
+  "image_attribution": "['<a href=\"https://maps.google.com/maps/contrib/105610711490865439378\">Chuyi Cui</a>']"
+}, {
+  "name": "The Garden Table",
+  "address": "908 E Westfield Blvd, Indianapolis, IN 46220, USA",
+  "key": "ChIJU_j8La9Ta4gRcUJFRKAjxcY",
+  "image_attribution": "['<a href=\"https://maps.google.com/maps/contrib/100546716197940745244\">David Arnold</a>']"
+}, {
+  "name": "317 Burger",
+  "address": "915 E Westfield Blvd, Indianapolis, IN 46220, USA",
+  "key": "ChIJ42UqL69Ta4gRmiTZ70vrWZo",
+  "image_attribution": "['<a href=\"https://maps.google.com/maps/contrib/103488603058038426756\">Paul Gerhardt</a>']"
+}, {
+  "name": "Broad Ripple Brewpub",
+  "address": "842 E 65th St, Indianapolis, IN 46220, USA",
+  "key": "ChIJv99c9qhTa4gRSkzRQRWtORM",
+  "image_attribution": "['<a href=\"https://maps.google.com/maps/contrib/118027059925145010760\">Broad Ripple Brewpub</a>']"
 }];
 export default INDYPlaces;
